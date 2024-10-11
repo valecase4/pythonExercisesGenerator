e2f3fd78b31c803871ac44fc225b461acc830a3f
valecase4 <valeriocaselli50@gmail.com>
2024-10-11 15:50:42 +0200

    Created the first view: test for an exercise.

---
+++ flaskr/__init__.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+
+def create_app(test_config=None):
+    app = Flask(__name__)
+    app.config.from_mapping(
+        SECRET_KEY = 'dev'
+    )
+
+    if test_config is None:
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        app.config.from_mapping(test_config)
+
+    @app.route("/hello")
+    def hello():
+        return 'Hello, world!'
+    
+    @app.route("/exercise")
+    def exercise():
+        
+
+        return render_template("index.html")
+    
+    return app
